ee64e6eff2d937f67afab3f503fa2534b9a92c4c
Aqwam Hizbal Muhshiy <aqwam.hizbal@outlook.com>
2024-10-07 08:26:24 +0700

    change card subheadline color to improve accessibility

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -48,14 +48,7 @@ const CardDescription = React.forwardRef<
     HTMLParagraphElement,
     React.HTMLAttributes<HTMLParagraphElement>
 >(({ className, ...props }, ref) => (
-    <p
-        ref={ref}
-        className={cn(
-            "text-sm text-neutral-500 dark:text-neutral-400",
-            className
-        )}
-        {...props}
-    />
+    <p ref={ref} className={cn("text-sm text-white", className)} {...props} />
 ));
 CardDescription.displayName = "CardDescription";
 
